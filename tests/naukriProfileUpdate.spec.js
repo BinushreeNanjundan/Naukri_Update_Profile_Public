4d578138372e971ab6d42c54537ea812f8d4e02e
BinushreeNanjundan <binushree1991@gmail.com>
2025-08-06 10:46:26 +0530

    Update naukriProfileUpdate.spec.js

--- tests/naukriProfileUpdate.spec.js
+++ tests/naukriProfileUpdate.spec.js
@@ -31,15 +31,17 @@ console.log('email:', email);
 
 test.describe('Update Profile in Naukri', () => {
 
-  test.beforeEach(async ({ page }) => {
-
-   // await page.goto('https://www.naukri.com/')
-    
+  test.beforeEach(async ({ page }) => {   
+    try{
 await page.goto('https://www.naukri.com/', { waitUntil: 'load', timeout: 60000 });
-
     //click on the login button
     //await page.getByText('Login').click()
     await page.locator('#login_Layer').click()
+    }
+  catch(error){  
+    console.error('Error during beforeEach setup:', error);
+    throw error
+  }
   })
 
 
