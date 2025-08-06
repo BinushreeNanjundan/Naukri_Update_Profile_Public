6a7d583cf4fc6542eea4d179189f82d89ba9128b
BinushreeN <binushree1991@gmail.com>
2025-08-06 13:33:59 +0530

    created new test file

--- tests/testchatcovit.spec.js
+++ tests/testchatcovit.spec.js
@@ -1,12 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Update Profile in Naukri', () => {
-  test.only('Login to Naukri', async ({ browser }) => {
+  test('goto google.com', async ({ browser }) => {
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto('https://www.google.com/');
-    await page.locator('.gLFyf').click()
+    //await page.locator('.gLFyf').click()
      await page.locator('.gLFyf').fill("abc")
+console.log("Google searchd abc");
 
   })
 })
